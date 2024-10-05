14bd4e4c8daff5ddcf939c0b2874dcc37c17bff1
matthew-jang <matthew.jang@eagles.oc.edu>
2024-10-05 01:26:15 -0500

    get courses url fix

--- app/routes/course.routes.js
+++ app/routes/course.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const courseController = require('../controllers/course.controller'); // Adjust path as needed
 
 // Define routes
-router.get('/courses-t4', courseController.getAllCourses);
+router.get('/course-t4', courseController.getAllCourses);
 router.get('/course-t4/:id', courseController.getCourseById);
 router.post('/course-t4', courseController.createCourse);
 router.put('/course-t4/:id', courseController.updateCourse);
